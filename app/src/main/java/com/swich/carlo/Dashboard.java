1376c63d24d38f0cfeaac78b47801aef937c1af5
Himanshu72 <36104578+Himanshu72@users.noreply.github.com>
2020-03-02 00:16:17 +0530

    intial commit register and login completeed till now

---
+++ app/src/main/java/com/swich/carlo/Dashboard.java
@@ -0,0 +1,43 @@
+package com.swich.carlo;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Dashboard extends AppCompatActivity {
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        switch (id){
+            case R.id.logout:
+                SharedPreferences preferences = getSharedPreferences("MyPref", 0);
+                preferences.edit().remove("user").commit();
+                Intent i=new Intent(this,login.class);
+                startActivity(i);
+                finish();
+
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return  true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
+
+
+}
